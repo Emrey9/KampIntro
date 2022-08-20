5e3f6b80d99fcb64bae1cca639c874d8fb1fa6ec
Emre YILMAZ <218EE2190@isik.edu.tr>
2022-08-20 04:22:50 +0300

    4.gün , Odev 5

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, K>
+    {
+
+        KeyValuePair<T, K>[] items;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<T, K>[0];
+        }
+        public void Add(T item, K key)
+        {
+            KeyValuePair<T, K>[] temp = items;
+            items = new KeyValuePair<T, K>[items.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                items[i] = temp[i];
+            }
+            items[items.Length - 1] = new KeyValuePair<T, K>(item, key);
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>;
+            myDictionary.Add("Emre",1);
+
+        }
+    }
+}
